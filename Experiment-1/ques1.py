8041b4408c9aee04b0b255a2276090f1747ad392
shub0x-droid <csen46425@gmail.com>
2026-09-21 20:41:21 +0530

    Create ques1.py

---
+++ Experiment-1/ques1.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+marks = np.array([78, 85, 90, 62, 74, 88, 95, 81, 69, 87])
+
+print("Marks:", marks)
+print("Mean:", np.mean(marks))
+print("Median:", np.median(marks))
+print("Standard Deviation:", np.std(marks))
+print("Maximum:", np.max(marks))
+print("Minimum:", np.min(marks))
